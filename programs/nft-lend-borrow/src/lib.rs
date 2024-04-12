55b59a84c9923015b1ce1c02aefc2c3410d76d45
addularijen <166828910+addularijen@users.noreply.github.com>
2024-04-12 21:31:48 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("mSdjMV6uGTUztgVLczxUnd1emqNGKMPfB2dWu4HbxAH");
 
 #[program]
 pub mod nft_lend_borrow {
